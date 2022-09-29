39ca056aae5d94f732e1f0144a6a589af85adb9b
VernonVerns <smaverns@gmail.com>
2022-09-29 08:04:17 +0200

    first commit - final website

---
+++ src/components/Navigation.vue
@@ -0,0 +1,34 @@
+<script lang="ts" setup>
+    const reloadPage = () => {
+        window.location.reload();
+    }
+</script>
+<template>
+    <nav class="navbar navbar-expand-md navbar-light" id="navigation_bar">
+        <div class="container-fluid">
+             <a class="navbar-brand" href="#">
+                <img src="@/assets/img/icon-logo.png" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+           
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" @click="reloadPage">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#about_us">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#our_services" >Services</a>
+                    </li>
+                    <li class="nav-item contact-btn">
+                        <a class="nav-link main-btn" href="#contact_us" >Contact Us</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,127 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+	<div id="home_page">
+		<div class="hero-part">
+			<div class="content">
+				<div class="text-side">
+					<h3 class="scripted-style">Pariatur aspernatur</h3>
+					<h1 class="main-title">Lorem ipsum dolor sit.</h1>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem totam, aliquid, pariatur aspernatur inventore, voluptas modi eligendi impedit culpa dolorum sit magni! Quod ab, magnam hic architecto dignissimos dolor veniam!
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="about-us" id="about_us">
+			<div class="content">
+				<div class="row">
+					<div class="col-sm-7">
+						<div class="text-side">
+							<h3 class="scripted-style">Our profile <span>About us</span></h3>
+							<p>
+								Id, dolore facilis. Aliquid, inventore dolor vitae ad dignissimos asperiores quidem soluta, vel placeat et aliquam ea, ut repudiandae! Fugit, facere eveniet?
+								Doloribus officia quos, voluptates obcaecati neque nostrum velit animi! Nulla fugit dolores obcaecati vel nam a ducimus beatae tempora dolorum at! Dolor unde autem tenetur. Ab ipsam in inventore qui.
+							</p>
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus voluptate odio dicta reiciendis aspernatur nam! Maiores maxime numquam molestias, eveniet amet eaque error temporibus similique labore ea quae? Saepe, magnam?
+								Eos ut eaque sapiente architecto iusto alias facere numquam velit sunt voluptatibus magnam optio, debitis deserunt reprehenderit accusamus dolores earum voluptas quia impedit soluta ex similique illo! Totam, a officiis?
+								Minus ab debitis tempore expedita quae quos totam maxime explicabo sequi, assumenda porro, earum quisquam. Asperiores illum eos maiores, soluta voluptates aut, reprehenderit eum dignissimos qui quos impedit, quis tenetur?
+							</p>
+						</div>
+					</div>
+					<div class="col-sm-5">
+						<div class="img-side">
+							<img src="@/assets/img/logo.png" alt="Full Company Logo">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="our-services" id="our_serices">
+			<div class="content">
+				<div class="scripted-style">We add value <span>Our Services</span></div>
+
+				<div class="row gx-5 gy-5">
+					<div class="col-sm-4">
+						<div class="service-card">
+							<h4>Dolorem dolores aliquid illum</h4>
+							<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut accusamus accusantium quam facilis mollitia quo. Dolorem dolores aliquid illum reprehenderit, ad et laborum suscipit vitae rem accusamus a perferendis beatae!</p>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="service-card">
+							<h4>Dolorem dolores aliquid illum</h4>
+							<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut accusamus accusantium quam facilis mollitia quo. Dolorem dolores aliquid illum reprehenderit, ad et laborum suscipit vitae rem accusamus a perferendis beatae!</p>
+						</div>
+					</div>
+					<div class="col-sm-4">
+						<div class="service-card">
+							<h4>Dolorem dolores aliquid illum</h4>
+							<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aut accusamus accusantium quam facilis mollitia quo. Dolorem dolores aliquid illum reprehenderit, ad et laborum suscipit vitae rem accusamus a perferendis beatae!</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="interested">
+			<div class="content">
+				<h1 class="main-title">Are you interested yet?</h1>
+				<p>You have reached this far, are you ready to reach out?</p>
+				<a href="#contact_use" class="border-btn">Get In Touch</a>
+			</div>
+		</div>
+		<div class="contact-us" id="contact_us">
+			<div class="content">
+				<div class="scripted-style">Get in touch <span>Contact Us</span></div>
+				
+				<div class="row">
+					<div class="col-sm-6">
+						<div class="text-side">
+							<h4>Contact Details</h4>
+							<p>
+								<i class="fa fa-phone"></i><a href="tel:+27000000000">083 000 0000</a> | <a href="tel:+27000000000">073 982 7763</a>
+							</p>
+							<p>
+								<i class="fa fa-envelope"></i><a href="mailto:info@something.co.za">info@something.co.za</a>
+							</p>
+							<p>
+								<i class="fa fa-map-marker-alt"></i><a href="https://goo.gl/maps/1Z1Z1Z1Z1Z1Z1Z1Z9" target="_blank">1234 Street Name, City, Province, Country</a>
+							</p>
+
+							<div class="social-side">
+								<a href="#"><i class="fab fa-facebook-f"></i></a>
+								<a href="#"><i class="fab fa-twitter"></i></a>
+								<a href="#"><i class="fab fa-instagram"></i></a>
+								<a href="#"><i class="fab fa-linkedin-in"></i></a>
+							</div>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="map-side">
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3966.693468574554!2d3.398553314769809!3d6.465731995493409!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8f7f0f4b0a0b%3A0x7b3b3b3b3b3b3b3b!2sLagos%20State%20University%20Teaching%20Hospital!5e0!3m2!1sen!2sng!4v1625581000000!5m2!1sen!2sng" width="100%" height="400px" style="border:0;" loading="lazy"></iframe>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="footer-part">
+			<div class="content">
+				<div class="row">
+					<div class="col-sm-6">
+						<div class="text-side">
+							<p>&copy; 2021 Mlungiseleli Wempumelelo Consultancy. All Rights Reserved</p>
+						</div>
+					</div>
+					<div class="col-sm-6">
+						<div class="social-side">
+							<a href="#"><i class="fab fa-facebook-f"></i></a>
+							<a href="#"><i class="fab fa-twitter"></i></a>
+							<a href="#"><i class="fab fa-instagram"></i></a>
+							<a href="#"><i class="fab fa-linkedin-in"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
-</script>
